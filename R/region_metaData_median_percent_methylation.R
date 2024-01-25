db1fd2adfc3eafc38ff85c774670093ead44349d
blaisemariner17 <blaisemariner17@gmail.com>
2024-01-25 12:46:07 -0700

    add the ability for the function to remove the y axis title if user wants

--- R/region_metaData_median_percent_methylation.R
+++ R/region_metaData_median_percent_methylation.R
@@ -51,7 +51,7 @@ region_metaData_median_percent_methylation <- function(coverage_all_chr,
         xlim(c(0,1))+
         theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(),
               axis.text.x = element_blank(), axis.ticks.x = element_blank(),
-              axis.title.y = element_markdown(angle = 0, hjust = -1, vjust = 0.5),
+              axis.title.y = element_markdown(angle = 0, hjust = 1, vjust = 0.5),
               axis.title.x = element_blank(), plot.title = element_blank(),
               plot.margin = margin(0, 0, 0, 0, "cm"),
               axis.line.y=element_blank()

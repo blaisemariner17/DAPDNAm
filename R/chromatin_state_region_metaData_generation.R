1ea2e842ba5e5207001229b3e69fa78c5792a2da
blaisemariner17 <blaisemariner17@gmail.com>
2024-02-13 09:44:02 -0700

    adding the chromatin states region metadata generation function

--- R/chromatin_state_region_metaData_generation.R
+++ R/chromatin_state_region_metaData_generation.R
@@ -48,26 +48,6 @@ chromatin_state_region_metaData_generation <- function(regions,
       ))
     }
   }
-  
-  region_metaData$LINE1 <- 0
-  region_metaData$LINE1[grepl("L1", region_metaData$LINE_id)] <- 1
-  region_metaData$LINE2 <- 0
-  region_metaData$LINE2[grepl("L2", region_metaData$LINE_id)] <- 1
-  region_metaData$LINE3 <- 0
-  region_metaData$LINE3[grepl("L3", region_metaData$LINE_id)] <- 1
-  region_metaData$LINE_CR1 <- 0
-  region_metaData$LINE_CR1[grepl("CR1", region_metaData$LINE_id)] <- 1
-  
-  region_metaData$SINEC <- 0
-  region_metaData$SINEC[grepl("SINEC", region_metaData$SINE_id)] <- 1
-  region_metaData$SINE_MIR <- 0
-  region_metaData$SINE_MIR[grepl("MIR", region_metaData$SINE_id)] <- 1
-  region_metaData$SINE_tRNA <- 0
-  region_metaData$SINE_tRNA[grepl("tRNA", region_metaData$SINE_id)] <- 1
-  
-  region_metaData$TE <- 0
-  region_metaData$TE[region_metaData$LINE == 1 | region_metaData$SINE == 1] <- 1
-  
+
   return(region_metaData)
-  }
 }

346146c30e7e5f64b64da3149b63db5876a9ad10
blaisemariner17 <blaisemariner17@gmail.com>
2024-02-14 11:29:19 -0700

    adding human conserved function

---
+++ R/human_conserved_region_metaData_generation.R
@@ -0,0 +1,58 @@
+#'generate transposable element region metadata from regions of interest given provided annotation files
+#'
+#' @param regions regions of interest
+#' @param conserved_annotation annotation file
+#' @return Function returns region metadata of interest
+#' @export human_conserved_region_metaData_generation
+
+human_conserved_region_metaData_generation <- function(regions,
+                                       conserved_annotation
+) {
+  chromosome <- unique(regions$chr)
+  if (length(chromosome)>1){stop("Run this function in parallel or one chromosome at a time.")}
+
+  i = 1
+  for (region in rownames(regions)) {
+
+    ph <- stringr::str_split(region, stringr::fixed("_"))
+    start <-  as.numeric(ph[[1]][2])
+    end <-  as.numeric(ph[[1]][3])
+
+    # this is
+    rangesA <- IRanges::IRanges(start, end)
+    rangesB <- IRanges::IRanges(conserved_annotation$start, conserved_annotation$end)
+
+    ov <- GenomicRanges::countOverlaps(rangesB, rangesA, type="any")>0
+    hit <- conserved_annotation[ov,]
+
+    if (nrow(hit) > 0){conserved_region = 1} else {conserved_region = 0}
+
+    if (i == 1){
+      region_metaData <- data.frame("region" = region,"conserved_region"=conserved_region)
+      i = 2
+    } else {
+      region_metaData <- rbind(region_metaData, data.frame("region" = region,"conserved_region"=conserved_region))
+    }
+  }
+
+  region_metaData$LINE1 <- 0
+  region_metaData$LINE1[grepl("L1", region_metaData$LINE_id)] <- 1
+  region_metaData$LINE2 <- 0
+  region_metaData$LINE2[grepl("L2", region_metaData$LINE_id)] <- 1
+  region_metaData$LINE3 <- 0
+  region_metaData$LINE3[grepl("L3", region_metaData$LINE_id)] <- 1
+  region_metaData$LINE_CR1 <- 0
+  region_metaData$LINE_CR1[grepl("CR1", region_metaData$LINE_id)] <- 1
+
+  region_metaData$SINEC <- 0
+  region_metaData$SINEC[grepl("SINEC", region_metaData$SINE_id)] <- 1
+  region_metaData$SINE_MIR <- 0
+  region_metaData$SINE_MIR[grepl("MIR", region_metaData$SINE_id)] <- 1
+  region_metaData$SINE_tRNA <- 0
+  region_metaData$SINE_tRNA[grepl("tRNA", region_metaData$SINE_id)] <- 1
+
+  region_metaData$TE <- 0
+  region_metaData$TE[region_metaData$LINE == 1 | region_metaData$SINE == 1 | region_metaData$DNA == 1| region_metaData$LTR == 1] <- 1
+
+  return(region_metaData)
+}

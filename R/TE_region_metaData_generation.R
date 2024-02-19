6b00b0e9241d2a62c041a357e88cca9057fc7712
blaisemariner17 <blaisemariner17@gmail.com>
2024-02-19 13:41:36 -0700

    adding retrotransposon and dna transposon return columsn to the te region metdata function

--- R/TE_region_metaData_generation.R
+++ R/TE_region_metaData_generation.R
@@ -4,13 +4,15 @@
 #' @param transposons_annotation TE annotation file
 #' @param sw_score_cutoff the minimum sw score that will be queried here
 #' @param expanded_search_if_nec expanding the search if necessary for region overlaps
+#' @param return_classes if TRUE, returns if the region overlaps with transposon classes (LINE1, LINE2, SINE_tRNA, etc.)
 #' @return Function returns region metadata of interest
 #' @export TE_region_metaData_generation
 
 TE_region_metaData_generation <- function(regions,
                                        transposons_annotation,
                                        sw_score_cutoff = 225,
-                                       expanded_search_if_nec = 500
+                                       expanded_search_if_nec = 50,
+                                       return_classes = FALSE
 ) {
   chromosome <- unique(regions$chr)
   if (length(chromosome)>1){stop("Run this function in parallel or one chromosome at a time.")}
@@ -55,31 +57,46 @@ TE_region_metaData_generation <- function(regions,
     if ("SINE" %in% hit$class){sine = 1; sine_id <- unique(hit$SINE_id[hit$SINE_id!=0])} else {sine = 0; sine_id <- 0}
 
     if (i == 1){
-      region_metaData <- data.frame("region" = region,"DNA"=dna,"LTR" = ltr,"LINE" = line,"SINE" = sine,"LINE_id" = paste(line_id, collapse = " & "),"SINE_id" = paste(sine_id, collapse = " & "))
+      region_metaData <- data.frame("region" = region,
+                                    "DNA transposon" = dna,
+                                    "LTR" = ltr,
+                                    "LINE" = line,"SINE" = sine,
+                                    "LINE_id" = paste(line_id, collapse = " & "),
+                                    "SINE_id" = paste(sine_id, collapse = " & "))
       i = 2
     } else {
-      region_metaData <- rbind(region_metaData, data.frame("region" = region,"DNA"=dna,"LTR" = ltr,"LINE" = line,"SINE" = sine,"LINE_id" = paste(line_id, collapse = " & "),"SINE_id" = paste(sine_id, collapse = " & ")))
+      region_metaData <- rbind(region_metaData, data.frame("region" = region,
+                                                           "DNA transposon"= dna,
+                                                           "LTR" = ltr,
+                                                           "LINE" = line,"SINE" = sine,
+                                                           "LINE_id" = paste(line_id, collapse = " & "),
+                                                           "SINE_id" = paste(sine_id, collapse = " & ")))
     }
   }
 
-  region_metaData$LINE1 <- 0
-  region_metaData$LINE1[grepl("L1", region_metaData$LINE_id)] <- 1
-  region_metaData$LINE2 <- 0
-  region_metaData$LINE2[grepl("L2", region_metaData$LINE_id)] <- 1
-  region_metaData$LINE3 <- 0
-  region_metaData$LINE3[grepl("L3", region_metaData$LINE_id)] <- 1
-  region_metaData$LINE_CR1 <- 0
-  region_metaData$LINE_CR1[grepl("CR1", region_metaData$LINE_id)] <- 1
+  if (return_classes){
+    region_metaData$LINE1 <- 0
+    region_metaData$LINE1[grepl("L1", region_metaData$LINE_id)] <- 1
+    region_metaData$LINE2 <- 0
+    region_metaData$LINE2[grepl("L2", region_metaData$LINE_id)] <- 1
+    region_metaData$LINE3 <- 0
+    region_metaData$LINE3[grepl("L3", region_metaData$LINE_id)] <- 1
+    region_metaData$LINE_CR1 <- 0
+    region_metaData$LINE_CR1[grepl("CR1", region_metaData$LINE_id)] <- 1
 
-  region_metaData$SINEC <- 0
-  region_metaData$SINEC[grepl("SINEC", region_metaData$SINE_id)] <- 1
-  region_metaData$SINE_MIR <- 0
-  region_metaData$SINE_MIR[grepl("MIR", region_metaData$SINE_id)] <- 1
-  region_metaData$SINE_tRNA <- 0
-  region_metaData$SINE_tRNA[grepl("tRNA", region_metaData$SINE_id)] <- 1
+    region_metaData$SINEC <- 0
+    region_metaData$SINEC[grepl("SINEC", region_metaData$SINE_id)] <- 1
+    region_metaData$SINE_MIR <- 0
+    region_metaData$SINE_MIR[grepl("MIR", region_metaData$SINE_id)] <- 1
+    region_metaData$SINE_tRNA <- 0
+    region_metaData$SINE_tRNA[grepl("tRNA", region_metaData$SINE_id)] <- 1
+  }
 
   region_metaData$TE <- 0
   region_metaData$TE[region_metaData$LINE == 1 | region_metaData$SINE == 1 | region_metaData$DNA == 1| region_metaData$LTR == 1] <- 1
+
+  region_metaData$Retrotransposon <- 0
+  region_metaData$Retrotransposon[region_metaData$LINE == 1 | region_metaData$SINE == 1 | region_metaData$LTR == 1] <- 1
 
   return(region_metaData)
 }

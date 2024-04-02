337855b01a1176f317e5fc7f0a1e0324be40c825
blaisemariner17 <blaisemariner17@gmail.com>
2024-04-02 13:40:50 -0700

    adding a clock simulating function

--- R/simulate_clock.R
+++ R/simulate_clock.R
@@ -31,7 +31,7 @@ clock_simulate <- function(alph,
   perc_meth_rand <- perc_meth[rownames(perc_meth) %in% sampling,]
   region_metaData_rand <- region_metaData[region_metaData$region %in% sampling,]
 
-  if (sampling == NULL){
+  if (is.null(sampling)){
     sampling <- sample(unique(metaData$dog_id), as.integer(.1*length(unique(metaData$dog_id))))
   }
 

8b9a8394ab6c8957f6a30e94c2441432059a7102
blaisemariner17 <blaisemariner17@gmail.com>
2024-04-09 11:41:21 -0700

    fixing build clock

--- R/simulate_clock.R
+++ R/simulate_clock.R
@@ -10,6 +10,7 @@
 #' @param perc_meth_prmtr imputed promoter percent methylation matrix
 #' @param region_metaData_cpgisl promoter region metaData
 #' @param perc_meth_cpgisl imputed promoter percent methylation matrix
+#' @param frac_testing testing fraction if you want
 #' @param sampling manual sampling if u want
 #' @return Function returns a list of the simulated clock results
 #' @export clock_simulate
@@ -33,67 +34,67 @@ clock_simulate <- function(alph,
   region_metaData_rand <- region_metaData[region_metaData$region %in% sampling_rand,]
 
   if (is.null(sampling)){
-    sampling <- sample(unique(metaData$dog_id), as.integer(frac_testing*length(unique(metaData$dog_id))))
+    sampling <- sample(unique(metaData$lid_pid), as.integer(frac_testing*length(unique(metaData$lid_pid))))
   }
 
   list_res <- DAPDNAm::build_clock(alph,
                                    metaData,
                                    region_metaData,
                                    perc_meth,
-                                   test_samples = colnames(perc_meth) %in% metaData$lid_pid[metaData$dog_id %in% sampling]
+                                   test_samples = sampling
   )
 
-  list_res_TE <- DAPDNAm::build_clock(alph,
-                                      metaData,
-                                      region_metaData_te,
-                                      perc_meth_te,
-                                      test_samples = colnames(perc_meth_te) %in% metaData$lid_pid[metaData$dog_id %in% sampling]
-  )
-
-  list_res_PR <- DAPDNAm::build_clock(alph,
-                                      metaData,
-                                      region_metaData_prmtr,
-                                      perc_meth_prmtr,
-                                      test_samples = colnames(perc_meth_prmtr) %in% metaData$lid_pid[metaData$dog_id %in% sampling]
-  )
-
-  list_res_CpGIs <- DAPDNAm::build_clock(alph,
-                                         metaData,
-                                         region_metaData_cpgisl,
-                                         perc_meth_cpgisl,
-                                         test_samples = colnames(perc_meth_cpgisl) %in% metaData$lid_pid[metaData$dog_id %in% sampling]
-  )
-
-  list_res_rand <- DAPDNAm::build_clock(alph,
-                                        metaData,
-                                        region_metaData_rand,
-                                        perc_meth_rand,
-                                        test_samples = colnames(perc_meth_rand) %in% metaData$lid_pid[metaData$dog_id %in% sampling]
-  )
+  # list_res_TE <- DAPDNAm::build_clock(alph,
+  #                                     metaData,
+  #                                     region_metaData_te,
+  #                                     perc_meth_te,
+  #                                     test_samples = sampling
+  # )
+  #
+  # list_res_PR <- DAPDNAm::build_clock(alph,
+  #                                     metaData,
+  #                                     region_metaData_prmtr,
+  #                                     perc_meth_prmtr,
+  #                                     test_samples = sampling
+  # )
+  #
+  # list_res_CpGIs <- DAPDNAm::build_clock(alph,
+  #                                        metaData,
+  #                                        region_metaData_cpgisl,
+  #                                        perc_meth_cpgisl,
+  #                                        test_samples = sampling
+  # )
+  #
+  # list_res_rand <- DAPDNAm::build_clock(alph,
+  #                                       metaData,
+  #                                       region_metaData_rand,
+  #                                       perc_meth_rand,
+  #                                       test_samples = sampling
+  # )
 
   list_res[['metaData']]$regions <- "All"
-  list_res_TE[['metaData']]$regions <- "TE"
-  list_res_PR[['metaData']]$regions <- "Promoter"
-  list_res_CpGIs[['metaData']]$regions <- "CpG_island"
-  list_res_rand[['metaData']]$regions <- "Random"
+  # list_res_TE[['metaData']]$regions <- "TE"
+  # list_res_PR[['metaData']]$regions <- "Promoter"
+  # list_res_CpGIs[['metaData']]$regions <- "CpG_island"
+  # list_res_rand[['metaData']]$regions <- "Random"
 
   list_res[['region_metaData']]$regions <- "All"
-  list_res_TE[['region_metaData']]$regions <- "TE"
-  list_res_PR[['region_metaData']]$regions <- "Promoter"
-  list_res_CpGIs[['region_metaData']]$regions <- "CpG_island"
-  list_res_rand[['region_metaData']]$regions <- "Random"
+  # list_res_TE[['region_metaData']]$regions <- "TE"
+  # list_res_PR[['region_metaData']]$regions <- "Promoter"
+  # list_res_CpGIs[['region_metaData']]$regions <- "CpG_island"
+  # list_res_rand[['region_metaData']]$regions <- "Random"
 
   list_res[['clock_results']]$regions <- "All"
-  list_res_TE[['clock_results']]$regions <- "TE"
-  list_res_PR[['clock_results']]$regions <- "Promoter"
-  list_res_CpGIs[['clock_results']]$regions <- "CpG_island"
-  list_res_rand[['clock_results']]$regions <- "Random"
+  # list_res_TE[['clock_results']]$regions <- "TE"
+  # list_res_PR[['clock_results']]$regions <- "Promoter"
+  # list_res_CpGIs[['clock_results']]$regions <- "CpG_island"
+  # list_res_rand[['clock_results']]$regions <- "Random"
 
   all_res_list <- list()
-  all_res_list[["metaData"]] <- rbind(list_res[['metaData']], list_res_TE[['metaData']],list_res_PR[['metaData']], list_res_CpGIs[['metaData']],list_res_rand[['metaData']])
+  all_res_list[["metaData"]] <-list_res[['metaData']]# rbind(list_res[['metaData']], list_res_TE[['metaData']],list_res_PR[['metaData']], list_res_CpGIs[['metaData']],list_res_rand[['metaData']])
   all_res_list[["metaData"]]$alpha <- alph
   # all_res_list[["region_metaData"]] <- rbind(list_res[['region_metaData']], list_res_TE[['region_metaData']],list_res_PR[['region_metaData']], list_res_CpGIs[['region_metaData']],list_res_rand[['region_metaData']])
-  all_res_list[["clock_results"]] <- rbind(list_res[['clock_results']], list_res_TE[['clock_results']],list_res_PR[['clock_results']], list_res_CpGIs[['clock_results']],list_res_rand[['clock_results']])
+  all_res_list[["clock_results"]] <-list_res[['clock_results']]# rbind(list_res[['clock_results']], list_res_TE[['clock_results']],list_res_PR[['clock_results']], list_res_CpGIs[['clock_results']],list_res_rand[['clock_results']])
 
   return(all_res_list)
 }

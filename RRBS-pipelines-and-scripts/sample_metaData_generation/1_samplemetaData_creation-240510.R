eae1df5a3f88cf028a3f6acda89e71a7386de7ad
blaisemariner17 <blaisemariner17@gmail.com>
2024-05-13 10:26:42 -0700

    adding p1 region generation

--- RRBS-pipelines-and-scripts/sample_metaData_generation/1_samplemetaData_creation-240510.R
+++ RRBS-pipelines-and-scripts/sample_metaData_generation/1_samplemetaData_creation-240510.R
@@ -1,4 +1,4 @@
-#### Blaise Mariner 
+#### Blaise Mariner
 #### this is the script used to generate the sample metadata for all RRBS dogs
 ## 2024-05-09
 
@@ -26,18 +26,18 @@ if (dir.exists("metadata_samples") == FALSE) {dir.create("metadata_samples")}
 
 # # save out the lids if you need to pull from shiny
 # dap <- readRDS("/scratch/nsnyderm/dap_rrbs/bismarkBSseq.rds")
-# sampleNames(dap) <- paste(sub(".*.(lid_\\d+)_.*", "\\1", sampleNames(dap)), 
-#                           sub(".*_(pid_\\d+).*", "\\1", sampleNames(dap)), 
+# sampleNames(dap) <- paste(sub(".*.(lid_\\d+)_.*", "\\1", sampleNames(dap)),
+#                           sub(".*_(pid_\\d+).*", "\\1", sampleNames(dap)),
 #                           sep = "_")
 # lids <- gsub("(lid_\\d+)_.*", "\\1", sampleNames(dap))
 # write.csv(as.data.frame(lids), file = 'metadata_samples/lids.csv')
 
 # statsQC <- read.csv("../colData_metaData_final_240216/dap_rrbsQC_stats-240216.csv")
-statsQC <- read.csv("../colData_metaData_final_240216/dap_rrbsQC_stats.txt", sep = " ")
+statsQC <- read.csv("/scratch/nsnyderm/dap_rrbs/dap_rrbsQC_stats.txt", sep = " ")
 
 #### Load in the DAP, Shiny, and Clinical metadata
 #DAP data
-load("../colData_metaData_final_240216/DAP_2023_DogOverview_v1.0.RData")
+load("/home/bmarine2/colData_metaData_final_240216/DAP_2023_DogOverview_v1.0.RData")
 for (column_ in 1:ncol(DogOverview)) {
   DogOverview[,column_] <- as.character(haven::as_factor(DogOverview[,column_]))
 }
@@ -49,12 +49,12 @@ shiny_metaData$dap_elab_cohort <- gsub(" ", "_",shiny_metaData$dap_elab_cohort)
 shiny_metaData$dap_elab_cohort <- tolower(shiny_metaData$dap_elab_cohort)
 
 #clinical data
-clinical_metaData <- read.csv("../colData_metaData_final_240216/DAP_2023_SamplesResults_Metadata_v1.0.csv")
+clinical_metaData <- read.csv("/home/bmarine2/colData_metaData_final_240216/DAP_2023_SamplesResults_Metadata_v1.0.csv")
 clinical_metaData$dog_id <- as.character(clinical_metaData$dog_id)
 clinical_metaData <- clinical_metaData[clinical_metaData$Sample_Type %in% c("RRBS"),]
 
 #most up-to-date precision dogs
-pauls_precision <- read.csv("../colData_metaData_final_240216/precision240301-fromPaul.csv")
+pauls_precision <- read.csv("/home/bmarine2/colData_metaData_final_240216/precision240301-fromPaul.csv")
 pauls_precision$dog_id <- as.character(pauls_precision$dog_id)
 
 #### FILTER OUT THE LOW READS ####
@@ -89,16 +89,17 @@ for (lid in unique(metaData$lid)) {
 
 ###########################################################################################################################
 ###########################################################################################################################
-############################## some elab formatting is fucked up so this is needed ########################################
+############################## some elab formatting is fucked up so might be needed #######################################
 ###########################################################################################################################
 ###########################################################################################################################
+# 5/13/24 noah fixed this so we're good for now...
 
 # #note any NA values for dap sample ids
 # # sum(is.na(metaData$sid))
-# write.csv(x = metaData[is.na(metaData$sid),], file = "metadata_samples/na_shiny_metadata_searching/na_sample_id_shiny.csv")
+# write.csv(x = metaData[is.na(metaData$sid),], file = "metadata_samples/na_shiny_metaData_searching/na_sample_id_shiny.csv")
 # # Then you're going to use the eLab metadata shiny app to get the RDIDs and save that file and upload it here:
-# elab_wrong_format_rdid_file <- read.csv("metadata_samples/na_shiny_metadata_searching/rdid_na_samples.csv")
-# 
+# elab_wrong_format_rdid_file <- read.csv("metadata_samples/na_shiny_metaData_searching/rdid_na_samples.csv")
+#
 # #then we're going to scrape through with the appropriate id (this is the integer associated with DAP-####-#####D0p number, idk how else to get it besides this way)
 # library(httr)
 # library(jsonlite)
@@ -108,7 +109,7 @@ for (lid in unique(metaData$lid)) {
 # get_sid <- function(id) {
 #   sampleID=gsub(" ","%20",id)
 #   get_meta = paste0("https://us.elabjournal.com/api/v1/samples/",sampleID,"/meta")
-#   call_api <- httr::GET(url = get_meta, 
+#   call_api <- httr::GET(url = get_meta,
 #                         httr::add_headers(.headers = c('Authorization'= auth_token2, 'Accept' = 'text/json')))
 #   out = parse_json(httr::content(call_api))
 #   return(out$data[[1]]$value)
@@ -120,10 +121,10 @@ for (lid in unique(metaData$lid)) {
 #     DAP_idk_wtf_id_this_is <- elab_wrong_format_rdid_file$Source[elab_wrong_format_rdid_file$ID == rdid]
 #     correct_dap_sid <- get_sid(DAP_idk_wtf_id_this_is)
 #     print(correct_dap_sid)
-#     metaData$sid[metaData$lid == lid] <- correct_dap_sid 
+#     metaData$sid[metaData$lid == lid] <- correct_dap_sid
 #   }
 # }
-# 
+#
 # #this should now equal zero :)
 # sum(is.na(metaData$sid))
 
@@ -138,10 +139,10 @@ for (sid in metaData$sid) {
   clinical_dog_id <- unique(clinical_metaData$dog_id[clinical_metaData$DAP_Sample_ID == sid])
   if (length(clinical_dog_id) == 0) {clinical_dog_id = NA}
   metaData$clinical_dog_id[metaData$sid == sid] <- clinical_dog_id
-  
+
   shiny_dog_id <- unique(metaData$shiny_dog_id[metaData$sid == sid])
   if (length(shiny_dog_id) == 0){shiny_dog_id = NA}
-  
+
   if (!is.na(shiny_dog_id) & !is.na(clinical_dog_id)){
     if (shiny_dog_id == clinical_dog_id){
       metaData$dog_id[metaData$sid == sid] <- shiny_dog_id
@@ -153,7 +154,7 @@ for (sid in metaData$sid) {
   if (is.na(shiny_dog_id) & !is.na(clinical_dog_id)){
     metaData$dog_id[metaData$sid == sid] <- clinical_dog_id
   }
-  
+
   if (!is.na(shiny_dog_id) & is.na(clinical_dog_id)){
     metaData$dog_id[metaData$sid == sid] <- shiny_dog_id
   }
@@ -200,7 +201,7 @@ for (sid in unique(metaData$sid)){
   if (length(dog_id) == 0) {next}
   if (is.na(dog_id)) {next}
   if(!dog_id %in% DogOverview$dog_id){not_in_DogOv <- append(not_in_DogOv, dog_id);next}
-  
+
   #DO dems
   LifeStage_Class_at_HLES <- unique(na.omit(DogOverview$LifeStage_Class_at_HLES[DogOverview$dog_id == dog_id]));metaData$LifeStage_Class_at_HLES[metaData$sid == sid] <- LifeStage_Class_at_HLES
   Estimated_DOB <- DogOverview$Estimated_DOB[DogOverview$dog_id == dog_id];metaData$Estimated_DOB[metaData$sid == sid] <- Estimated_DOB
@@ -208,13 +209,13 @@ for (sid in unique(metaData$sid)){
   breed <- DogOverview$Breed[DogOverview$dog_id == dog_id]; metaData$Breed[metaData$sid == sid] <- breed
   Sex_class <- DogOverview$Sex_class_at_HLES[DogOverview$dog_id == dog_id]; metaData$Sex_class[metaData$sid == sid] <- Sex_class
   Breed_Size_Class_at_HLES <- DogOverview$Breed_Size_Class_at_HLES[DogOverview$dog_id == dog_id]; metaData$Breed_Size_Class_at_HLES[metaData$sid == sid] <- Breed_Size_Class_at_HLES
-  
+
   #clinical info
   if (!sid %in% clinical_metaData$DAP_Sample_ID){not_in_clinical <- append(not_in_clinical, sid);next}
   Sample_taken <- unique(na.omit(clinical_metaData$Sample_Collection_DateTime[clinical_metaData$DAP_Sample_ID == sid]))
   if (length(Sample_taken) > 0) {if (grepl("/",Sample_taken)){split <- str_split(Sample_taken, "/");month <- split[[1]][1];day <- split[[1]][2];split <- str_split(split[[1]][3], " ");year <- split[[1]][1];time <- split[[1]][2];Sample_taken <- paste0(year, "-", month, "-", day, " ", time)}}
   metaData$Sample_taken[metaData$sid == sid] <- Sample_taken
-  
+
   #the weight dem is a pain in my ass but we got it now
   weight_at_sample_collection <- clinical_metaData$Sample_Dog_Weight[clinical_metaData$DAP_Sample_ID == sid]
   weight_units <- (clinical_metaData$Sample_Dog_Weight_Units[clinical_metaData$DAP_Sample_ID == sid])
@@ -264,7 +265,7 @@ metaData$Breed_size[grepl("Standard",metaData$Breed_Size_Class_at_HLES)] <- "Sta
 metaData$Breed_size[grepl("Large",metaData$Breed_Size_Class_at_HLES)] <- "Large"
 metaData$Breed_size[grepl("Giant",metaData$Breed_Size_Class_at_HLES)] <- "Giant"
 
-################################################################################# DEMOGRAHIC OR CLINICAL CORRECTIONS: 
+################################################################################# DEMOGRAHIC OR CLINICAL CORRECTIONS:
 metaData$weight[metaData$dog_id == "94533"] <- 78 / 2.2
 metaData$weight[metaData$dog_id == "118580"] <- 46.4 / 2.2
 metaData$weight_units[metaData$dog_id == "94533"] <- "kg"
@@ -300,7 +301,7 @@ for (sid in metaData$sid[metaData$Estimated_DOB != "unknown"]){
 ###########################################################################################################################
 
 #predicted height
-predicted_height <- read.csv("../colData_metaData_final_240216/precision_dogs_N-837_bodysize.phenotypes-240214.csv")
+predicted_height <- read.csv("/home/bmarine2/colData_metaData_final_240216/precision_dogs_N-837_bodysize.phenotypes-240214.csv")
 predicted_height$dog_id <- as.character(predicted_height$id)
 
 for (sid in metaData$sid) {
@@ -312,6 +313,8 @@ for (sid in metaData$sid) {
     metaData$weight_over_height[metaData$sid == sid] <- weight_over_height
   }
 }
+
+# write.csv(file = "metadata_samples/unique_dog_ids.csv", x = unique(metaData[,c("dog_id")]))
 
 ###########################################################################################################################
 ###########################################################################################################################
@@ -348,11 +351,11 @@ pid_oi <- metaData[metaData$pid == "PID_10558" , ]
 pid_oi <- pid_oi[order(pid_oi$no_lid),]
 pid_oi[,c("pid","lid","dog_id")]
 #remove the identified swaps so far
-mismatches <- mismatches[!rownames(mismatches) %in% c("LID_1070233_PID_10558", "LID_1070236_PID_10558", 
+mismatches <- mismatches[!rownames(mismatches) %in% c("LID_1070233_PID_10558", "LID_1070236_PID_10558",
                                                       "LID_1071350_PID_10558", "LID_1070234_PID_10558"
-                                                      ),]
+),]
 
-pool_oi <- mismatches[mismatches$pid == "PID_10558"]
+pool_oi <- mismatches[mismatches$pid == "PID_10558",]
 dog_id_oi = "97916"
 pool_oi[pool_oi$dog_id == dog_id_oi | pool_oi$tophit == dog_id_oi| pool_oi$secondhit == dog_id_oi | pool_oi$thirdhit ==dog_id_oi,]
 #swaps identified 5/10/24 blm
@@ -365,29 +368,7 @@ metaData$lid_pid[metaData$lid_pid == "LID_1071350_PID_10558" & metaData$lid == "
 
 #swaps in pid 10559
 pool_oi <- mismatches[mismatches$pid == "PID_10559"
-           ,]
-for (dog_id in pool_oi$dog_id){
-  mismatch_hit <- pool_oi[pool_oi$tophit == dog_id,]
-  if (nrow(mismatch_hit) > 0){
-    print(mismatch_hit)
-  }
-}
-
-#swaps in pid 10560
-pool_oi <- mismatches[mismatches$pid == "PID_10560"
-           ,]
-for (dog_id in pool_oi$dog_id){
-  mismatch_hit <- pool_oi[pool_oi$tophit == dog_id,]
-  if (nrow(mismatch_hit) > 0){
-    print(mismatch_hit)
-  }
-}
-
-# metaData$lid_pid[metaData$lid_pid == "LID_1071346_PID_10560" & metaData$lid == "LID_1071346"] <- "_LID_1071349_PID_10560"
-
-#swaps in pid 10562
-pool_oi <- mismatches[mismatches$pid == "PID_10562"
-           ,]
+                      ,]
 for (dog_id in pool_oi$dog_id){
   mismatch_hit <- pool_oi[pool_oi$tophit == dog_id,]
   if (nrow(mismatch_hit) > 0){
@@ -407,34 +388,33 @@ rownames(metaData) <- metaData$lid_pid <- gsub("_LID" , "LID", metaData$lid_pid)
 ###########################################################################################################################
 ###########################################################################################################################
 
-write.csv(file = "metadata_samples/metaData_swaps_lidpid_dog_id_tophit.csv", x = metaData_swaps)
-
-metaData <- metaData[!metaData$lid_pid %in% metaData_swaps$lid_pid,]
+metaData <- metaData[!metaData$lid_pid %in% mismatches$lid_pid,]
+metaData$Cohort_group[is.na(metaData$Cohort_group)] <- "idk"
 
 write_rds("metadata_samples/ALL-DAP-metaData-240124.rds", x = metaData)
 
-p1 <- metaData_[metaData_$Cohort == "precision_1",]
+metaData <- metaData[metaData$Cohort_group == "Precision",]
+
+p1 <- metaData[metaData$Cohort == "precision_1",]
 for (dog_id in p1$dog_id[duplicated(p1$dog_id)]){
   min_reads <- min(p1$reads[p1$dog_id==dog_id])
   p1 <- p1[!(p1$dog_id == dog_id & p1$reads == min_reads),]
 }
 dim(p1)
-write_rds("metadata_samples/P1-DAP-metaData-240510.rds", x = p1)
+readr::write_rds("metadata_samples/P1-DAP-metaData-240510.rds", x = p1)
 
-p2 <- metaData_[metaData_$Cohort == "precision_2",]
+p2 <- metaData[metaData$Cohort == "precision_2",]
 for (dog_id in p2$dog_id[duplicated(p2$dog_id)]){
   min_reads <- min(p2$reads[p2$dog_id==dog_id])
   p2 <- p2[!(p2$dog_id == dog_id & p2$reads == min_reads),]
 }
 dim(p2)
-write_rds("metadata_samples/P2-DAP-metaData-240510.rds", x = p2)
+readr::write_rds("metadata_samples/P2-DAP-metaData-240510.rds", x = p2)
 
-p3 <- metaData_[metaData_$Cohort == "precision_3",]
+p3 <- metaData[metaData$Cohort == "precision_3",]
 for (dog_id in p3$dog_id[duplicated(p3$dog_id)]){
   min_reads <- min(p3$reads[p3$dog_id==dog_id])
   p3 <- p3[!(p3$dog_id == dog_id & p3$reads == min_reads),]
 }
 dim(p3)
-write_rds("metadata_samples/P3-DAP-metaData-240510.rds", x = p3)
-
-triad <- metaData[!grepl("precision", metaData$Cohort),]
+readr::write_rds("metadata_samples/P3-DAP-metaData-240510.rds", x = p3)

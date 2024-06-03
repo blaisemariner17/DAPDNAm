ab2f908c696c7c9ce9d386ff41440aa96139e996
blaisemariner17 <blaisemariner17@gmail.com>
2024-06-03 16:32:27 -0700

    more

--- Workflows/meQTL/1_sampleVCFs.sh
+++ Workflows/meQTL/1_sampleVCFs.sh
@@ -8,14 +8,14 @@ module load anaconda/py3
 source activate bismark
 module load trim_galore/0.4.0
 
-sampleID=/scratch/bmarine2/Precision123-240506/QTL/lid_pids.txt
-genome_path=/scratch/vsohrab/dap_map_canfam4/kiddlabshare.med.umich.edu/public-data/UU_Cfam_GSD_1.0-Y/UU_Cfam_GSD_1.0_ROSY.fa
+sample=/scratch/bmarine2/Precision123-240506/QTL/lid_pids.txt
+path_ref=/scratch/vsohrab/dap_map_canfam4/kiddlabshare.med.umich.edu/public-data/UU_Cfam_GSD_1.0-Y/UU_Cfam_GSD_1.0_ROSY.fa
 
 out_path=/scratch/bmarine2/Precision123--240506/QTL/sample_bams
 fastq_path=/scratch/nsnyderm/dap_rrbs/trimmed
 
-bismark --genome_folder ${genome_path} --non_directional -o ${out_path} --score_min L,0,-0.6 -R 12 --parallel 4 ${fastq_path}/${sampleID}*trimmed.fq.gz
+bismark --genome_folder ${path_ref} --non_directional -o ${out_path} --score_min L,0,-0.6 -R 12 --parallel 4 ${fastq_path}/${sample}*trimmed.fq.gz
 
-bismark_methylation_extractor -p -o ${cov_path} --no_overlap --bedGraph --comprehensive --parallel 10 --merge_non_CpG --genome_folder ${genome_path} ${out_path}/${sampleID}*.bam
+bismark_methylation_extractor -p -o ${cov_path} --no_overlap --bedGraph --comprehensive --parallel 10 --merge_non_CpG --genome_folder ${path_ref} ${out_path}/${sample}*.bam
 
 conda deactivate

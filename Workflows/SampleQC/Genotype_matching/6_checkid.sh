143a71c9cf99e7df679c2b4abce828a58dbd3c62
blaisemariner17 <blaisemariner17@gmail.com>
2024-06-04 07:06:11 -0700

    updating the checkid script

--- Workflows/SampleQC/Genotype_matching/6_checkid.sh
+++ Workflows/SampleQC/Genotype_matching/6_checkid.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-#SBATCH -G a100:2
-#SBATCH -t 0-2
+
+#SBATCH -N 1            # number of nodes
+#SBATCH -c 48           # number of cores 
 #SBATCH --mem=100G
-#SBATCH --cpus-per-task=12
+#SBATCH -t 0-20:00:00   # time in d-hh:mm:ss
+#SBATCH -p general      # partition 
+#SBATCH -q public       # QOS
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
 
 sampleID=/scratch/nsnyderm/dap_rrbs/lid_pid
 
@@ -12,7 +16,7 @@ vcf_path=/scratch/bmarine2/Triad_mapping/nvidia_fq2bam_mapping/DAP_Triad_and_Pre
 fastq_trim=/scratch/nsnyderm/dap_rrbs/trimmed
 sort_path=/scratch/nsnyderm/dap_rrbs/sort_bam
 
-out_path=/scratch/bmarine2/Triad_mapping/check_out
+out_path=/scratch/bmarine2/Triad_mapping/checkid
 
 module load bowtie2-2.4.2-gcc-11.2.0
 module load samtools-1.13-gcc-11.2.0
@@ -22,10 +26,8 @@ module load samtools-1.13-gcc-11.2.0
 
 export PATH=$PATH:/scratch/nsnyderm/programs/FastQC:/scratch/nsnyderm/programs/bin:/scratch/nsnyderm/programs/TrimGalore-0.6.6:/scratch/nsnyderm/programs/bismark2/Bismark-0.24.0
 
-if test -f ${sort_path}/${sampleID}.sort.bam
-then 
-       	qtltools_path=/scratch/nsnyderm/programs/QTLtools_1.2_CentOS7.8_x86_64
-       	${qtltools_path}/QTLtools mbv --vcf ${vcf_path} \
-                --bam ${sort_path}/${sampleID}.sort.bam \
-                --out ${out_path}/${sampleID}.match
+qtltools_path=/scratch/nsnyderm/programs/QTLtools_1.2_CentOS7.8_x86_64
+	${qtltools_path}/QTLtools mbv --vcf ${vcf_path} \
+	--bam ${sort_path}/${sampleID}.sort.bam \
+	--out ${out_path}/${sampleID}.match
 
--- Workflows/meQTL/1_sampleVCFs.sh
+++ Workflows/meQTL/1_sampleVCFs.sh
@@ -4,9 +4,11 @@
 #SBATCH --mem=100G
 #SBATCH --cpus-per-task=12
 
-module load anaconda/py3
-source activate bismark
-module load trim_galore/0.4.0
+module load bowtie2-2.4.2-gcc-11.2.0
+module load samtools-1.13-gcc-11.2.0
+module load py-cutadapt-2.10-gcc-11.2.0
+
+export PATH=$PATH:/scratch/nsnyderm/programs/FastQC:/scratch/nsnyderm/programs/bin:/scratch/nsnyderm/programs/TrimGalore-0.6.6:/scratch/nsnyderm/programs/bismark2/Bismark-0.24.0
 
 sample=/scratch/bmarine2/Precision123-240506/QTL/lid_pids.txt
 path_ref=/scratch/vsohrab/dap_map_canfam4/kiddlabshare.med.umich.edu/public-data/UU_Cfam_GSD_1.0-Y/UU_Cfam_GSD_1.0_ROSY.fa
@@ -17,5 +19,3 @@ fastq_path=/scratch/nsnyderm/dap_rrbs/trimmed
 bismark --genome_folder ${path_ref} --non_directional -o ${out_path} --score_min L,0,-0.6 -R 12 --parallel 4 ${fastq_path}/${sample}*trimmed.fq.gz
 
 bismark_methylation_extractor -p -o ${cov_path} --no_overlap --bedGraph --comprehensive --parallel 10 --merge_non_CpG --genome_folder ${path_ref} ${out_path}/${sample}*.bam
-
-conda deactivate

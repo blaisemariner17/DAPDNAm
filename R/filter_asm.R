e1cb6f69019c68c0e4fadb74e97a6d4873697d8d
blaisemariner17 <blaisemariner17@gmail.com>
2024-10-30 12:15:01 -0700

    renaming the function to make more sense conseptually

--- R/filter_asm.R
+++ R/filter_asm.R
@@ -2,9 +2,9 @@
 #'
 #' @param asm_res_oi the .asm file that you'd like to filter
 #' @return returns the snp cpg combinations with allele-specific methylation
-#' @export filter_asm
+#' @export sites_with_one_instance_of_asm
 
-filter_asm <- function(asm_res_oi){
+sites_with_one_instance_of_asm <- function(asm_res_oi){
   asm_snpcpg <- unique(asm_res_oi$Chr_SNP_C[asm_res_oi$ASM == TRUE])
   asm_res_oi <- asm_res_oi[asm_res_oi$Chr_SNP_C %in% asm_snpcpg,]
   return(asm_res_oi)

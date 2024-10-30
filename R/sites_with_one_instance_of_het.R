1ba9ea5252d662fa94a42cc033f200bbabf59041
blaisemariner17 <blaisemariner17@gmail.com>
2024-10-30 12:51:53 -0700

    adding the heterozygous rule, not the asm rule

--- R/sites_with_one_instance_of_het.R
+++ R/sites_with_one_instance_of_het.R
@@ -2,9 +2,9 @@
 #'
 #' @param asm_res_oi the .asm file that you'd like to filter
 #' @return returns the snp cpg combinations with allele-specific methylation
-#' @export sites_with_one_instance_of_asm
+#' @export sites_with_one_instance_of_het
 
-sites_with_one_instance_of_asm <- function(asm_res_oi){
+sites_with_one_instance_of_het <- function(asm_res_oi){
   asm_snpcpg <- unique(asm_res_oi$Chr_SNP_C[asm_res_oi$Allele1 != asm_res_oi$Allele2])
   asm_res_oi <- asm_res_oi[asm_res_oi$Chr_SNP_C %in% asm_snpcpg,]
   return(asm_res_oi)

0205c961a3fb8b04de6c3bda39331e85beeced72
blaisemariner17 <blaisemariner17@gmail.com>
2025-01-17 10:56:28 -0700

    updating SNP and pqlseq meQTL scripts. The structure of submitting them is different so removing the sbatch scripts. They are now set to be pushed to slurm in arrays of 1000 for each chromosome

--- Workflows/meQTL_pqlseq/process_pqlseq_res.sh
+++
@@ -1,8 +0,0 @@
-
-for i in {1..38}
-do
-	awk 'FNR>1' pqlseq_results/pqlseq_res${i}_* > processed_files/all_pqlseq_res${i}.tsv
-done
-
-cat processed_files/all_pqlseq_res*.tsv > all_pqlseq_res.tsv
-rm processed_files/*
--- Workflows/meQTL_pqlseq/region_metaData_SNP-241114.R
+++ Workflows/meQTL_pqlseq/region_metaData_SNP-241114.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env /packages/apps/spack/18/opt/spack/gcc-11.2.0/r-4.2.2-kpl/bin/Rscript
-#### Blaise Mariner 
+#### Blaise Mariner
 ### this script generates the region metaData such that there is a way to better understand the reigons that are differentially methylated with different demographics
 ## for questions contact bmarine2@asu.edu or blaisemariner17@gmail.com
 ## version of R 4.2.2 needed for bsseq as of the date below
@@ -9,8 +9,6 @@
 
 #### GLOBALS ####
 # this scrip requires at least 12 cores
-cores_ <- 20
-
 #### END GLOBALS ####
 
 library_list <- c(
@@ -37,6 +35,13 @@ theme_blaise <- theme(axis.text.x = element_text(angle=0),      plot.title = ele
 # this sets the working directory to this script's path
 if (isRStudio <- Sys.getenv("RSTUDIO") == "1"){
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+  n_cores = 4
+  batch = 1000
+} else {
+  args <- commandArgs(trailingOnly=TRUE)
+
+  batch <- args[1]
+  n_cores = 12
 }
 print(getwd())
 
@@ -56,8 +61,8 @@ chromatin_states_annotation$name[chromatin_states_annotation$name == "13"] <- "C
 
 gtf_trans <- read.csv("/home/bmarine2/GENOME-ANNOTATION-FILE/rmsk.txt", header = F, sep = "\t")
 colnames(gtf_trans) <- c('bin', 'swScore', 'milliDiv', 'milliDel', 'milliIns'	,
-                                      'seqnames', 'start', 'end', 'genoLeft', 'strand'	,
-                                      'repName', 'class', 'repFamily', 'repStart', 'repEnd',	'repLeft',	'id')
+                         'seqnames', 'start', 'end', 'genoLeft', 'strand'	,
+                         'repName', 'class', 'repFamily', 'repStart', 'repEnd',	'repLeft',	'id')
 gtf_trans <- gtf_trans[gtf_trans$seqnames %in% paste0("chr", c(1:38, "X")),]
 
 # gtf_trans$length<-gtf_trans$end-gtf_trans$start
@@ -71,8 +76,8 @@ gtf_trans <- gtf_trans[gtf_trans$seqnames %in% paste0("chr", c(1:38, "X")),]
 transposon_annotation <- gtf_trans[gtf_trans$class %in% c("LINE", "SINE", "LTR", "DNA",
                                                           "Simple_repeat","Low_complexity", "rRNA", "tRNA",
                                                           "Satellite","Unknown"
-                                                          ),]
-                                                          
+),]
+
 dim(transposon_annotation)
 
 rm(gtf_trans)
@@ -81,78 +86,221 @@ rm(gtf_trans)
 # remove.packages("DAPDNAm")
 if (! "DAPDNAm" %in% installed.packages()){devtools::install_github("blaisemariner17/DAPDNAm", force = TRUE)}
 library("DAPDNAm")
-#get the maxgaps I used from the previous loop
-maxgaps <- "250"
 
-maxgap = "250"
 if(dir.exists("metaData_SNPs")==F){dir.create("metaData_SNPs")}
 
-for (maxgap in maxgaps){
-  print(maxgap)
-  # regions <- read_rds(paste0("/scratch/bmarine2/BASELINE-PRECISION-240820/getcoverage_data_and_plots/maxGap_250_all_regions_1n_coverage.rds"))
-  
-  pqlseq_res <- readRDS("all_pqlseq_res_snp.rds")
+# regions <- read_rds(paste0("/scratch/bmarine2/BASELINE-PRECISION-240820/getcoverage_data_and_plots/maxGap_250_all_regions_1n_coverage.rds"))
 
-  tmp <- as.data.frame(do.call("rbind", stringr::str_split(pqlseq_res$region_snp, "_")))
-  colnames(tmp) <- c("chr", "start", "end", "snp")
-  tmp$region <- paste(tmp$chr, tmp$start, tmp$end, sep = "_")
-  tmp$start <- as.numeric(tmp$start)
-  tmp$end <- as.numeric(tmp$end)
-  tmp$region_length <- tmp$end - tmp$start
-  tmp$chr_snp <- paste0(tmp$chr, "_", tmp$snp)
-  pqlseq_res <- cbind(pqlseq_res, tmp)
-  rm(tmp)
-  
-  regions <- na.omit(unique(pqlseq_res[,c("chr_snp", "snp", "chr"), drop = F]))
-  regions$start <- as.numeric(regions$snp)
-  regions$end<- as.numeric(regions$snp)
-  
-  chrs=unique(regions$chr)
-  regions_list <- parallel::mclapply(chrs,
-                                     function(x, regions){
-                                       regions <- regions[regions$chr == x,]
-                                       rownames(regions) <- paste0(regions$chr, "_", regions$snp, "_", regions$snp)
-                                       return(regions)
-                                       },
-                                     regions = regions,
-                                     mc.cores =18)
-  names(regions_list) <- chrs
-  region_metaData_TE_list <- parallel::mclapply(regions_list, 
-                                                DAPDNAm::TE_region_metaData_generation,
-                                                transposons_annotation = transposon_annotation,
-                                                genome_gene_annotation = gtf,
-                                                sw_score_cutoff = 225,
-                                                mc.cores = 20)
-  
-  for (i in 1:length(region_metaData_TE_list)) {
-    if (i == 1){region_metaData_TE <- region_metaData_TE_list[[i]]} else {region_metaData_TE <- rbind(region_metaData_TE, region_metaData_TE_list[[i]])}
+if(TRUE){
+  library(genio)
+  for (chr in c(1:38,"X")){
+    if(file.exists(paste0("metaData_SNPs/chr",chr,"_SNPs.tsv"))){next}
+
+    print(paste0("chr", chr))
+
+    plink_data <- read_plink(paste0("/scratch/bmarine2/meQTL-241101-GEMMA/gemma_out/filtered_genotypes_chr",chr,".bed"))
+    plink_in <- plink_data$X
+    snp_bims <- unique(data.frame(plink_data$bim))
+
+    # snp_bims$chr_snp <- paste0(chr, "_", snp_bims$pos)
+    # rownames(plink_in) <- snp_bims$chr_snp
+    # plink_in <- t(plink_in)
+    #
+    # colnames(plink_in) <- gsub(paste0(chr,"_"), "", colnames(plink_in))
+    #
+    # if(chr == "X"){
+    #   colnames(plink_in) <- gsub(paste0("39_"), "", colnames(plink_in))
+    # }
+    #
+    # rownames(plink_in) <- gsub(".canfam4", "", rownames(plink_in))
+
+    rm(plink_data)
+
+    res_ <- snp_bims[,c("chr", "pos", "alt", "ref")]
+    colnames(res_) <- c("chr", "pos", "ALT", "REF")
+
+    if(chr == "X"){
+      res_$chr <- paste0("chrX")
+      res_$chr_snp <- paste0("chrX_", res_$pos)
+    } else {
+      res_$chr <- paste0("chr",res_$chr)
+      res_$chr_snp <- paste0(res_$chr, "_", res_$pos)
+    }
+    write.table(res_, paste0("metaData_SNPs/chr",chr,"_SNPs.tsv"))
+    rm(res_)
   }
-  
-  region_metaData_list <- parallel::mclapply(regions_list, 
-                                             DAPDNAm::region_metaData_generation,
-                                             genome_gene_annotation = gtf,
-                                             cpgisland_annotation = gtf_cpgisl,
-                                             mc.cores = 12)
-  
-  for (i in 1:length(region_metaData_list)) {
-    if (i == 1){region_metaData <- region_metaData_list[[i]]} else {region_metaData <- rbind(region_metaData, region_metaData_list[[i]])}
-  }
-  
-  region_metaData_ChromStates_list <- parallel::mclapply(regions_list, 
-                                             DAPDNAm::chromatin_state_region_metaData_generation,
-                                             chromatin_states_annotation = chromatin_states_annotation,
-                                             mc.cores = 12)
-  
-  for (i in 1:length(region_metaData_ChromStates_list)) {
-    if (i == 1){region_metaData_ChromStates <- region_metaData_ChromStates_list[[i]]} else {region_metaData_ChromStates <- rbind(region_metaData_ChromStates, region_metaData_ChromStates_list[[i]])}
-  }
-  
-  if (all(rownames(region_metaData_TE) == rownames(region_metaData))){region_metaData <- cbind(region_metaData, region_metaData_TE)} else {print("not all the transposable element rownames are the same!!!")}
-  if (all(rownames(region_metaData_ChromStates) == rownames(region_metaData))){region_metaData <- cbind(region_metaData, region_metaData_ChromStates)} else {print("not all the chromatin states rownames are the same!!!")}
-  # if (all(rownames(human_conserved_region_metaData) == rownames(region_metaData))){region_metaData <- cbind(region_metaData, human_conserved_region_metaData)} else {print("not all the human conserved regions rownames are the same!!!")}
-  
-  # rownames(region_metaData) <- region_metaData$region
-  
-  write_rds(x = region_metaData, file = paste0("metaData_SNPs/metaData_SNPs.rds"))
-  
 }
+
+for (chr in c(1:38,"X")){
+  print(paste0("chr",chr))
+  for (j in batch){
+    if(file.exists(paste0("SNP_metaData/chr",chr,"_SNP_batch",j,".tsv"))){next}
+    SNPs <- read.table(paste0("metaData_SNPs/chr",chr,"_SNPs.tsv"))
+    SNPs$chr_snp <- gsub("chrchr", "chr", SNPs$chr_snp)
+
+    groups <- split(1:nrow(SNPs),
+                    cut(1:nrow(SNPs), breaks = 1000, labels = FALSE)
+    )
+
+    # if(file.exists(paste0("SNP_metaData/SNP_batch",j,".tsv"))){next}
+
+    SNPs <- SNPs[groups[[j]],]
+    # SNPs <- SNPs[SNPs$region %in% c("chr3_42182260_42182338", "chr3_42237342_42237342", "chr3_42362631_42362768", "chr3_42385646_42385881" ,
+    #                                                           "chr3_42386355_42386458", "chr3_42416665_42416668", "chr3_42465880_42466230", "chr3_42469976_42470076"),]
+
+    regions <- na.omit(unique(SNPs[,c("chr_snp", "pos", "chr", "REF", "ALT"), drop = F]))
+    colnames(regions) <- c("chr_snp", "snp", "chr", "REF", "ALT")
+    regions$start <- as.numeric(regions$snp)
+    regions$end<- as.numeric(regions$snp)
+
+    chrs=unique(regions$chr)
+    regions_list <- parallel::mclapply(chrs,
+                                       function(x, regions){
+                                         regions <- regions[regions$chr == x,]
+                                         rownames(regions) <- paste0(regions$chr, "_", regions$snp, "_", regions$snp)
+                                         return(regions)
+                                       },
+                                       regions = regions,
+                                       mc.cores=n_cores)
+    names(regions_list) <- chrs
+
+    gtf_oi <- gtf[seqnames(gtf) %in% chrs,]
+    transposon_annotation_oi <- transposon_annotation[transposon_annotation$seqnames %in% chrs,]
+
+    SNP_metaData_TE_list <- parallel::mclapply(regions_list,
+                                               DAPDNAm::TE_region_metaData_generation,
+                                               transposons_annotation = transposon_annotation_oi,
+                                               genome_gene_annotation = gtf_oi,
+                                               sw_score_cutoff = 225,
+                                               mc.cores=n_cores)
+
+    for (i in 1:length(SNP_metaData_TE_list)) {
+      if (i == 1){SNP_metaData_TE <- SNP_metaData_TE_list[[i]]} else {SNP_metaData_TE <- rbind(SNP_metaData_TE, SNP_metaData_TE_list[[i]])}
+    }
+
+    gtf_cpgisl_oi <- gtf_cpgisl[gtf_cpgisl$V2 %in% chrs,]
+
+    SNP_metaData_list <- parallel::mclapply(regions_list,
+                                            DAPDNAm::region_metaData_generation,
+                                            genome_gene_annotation = gtf_oi,
+                                            cpgisland_annotation = gtf_cpgisl_oi,
+                                            mc.cores=n_cores)
+
+    for (i in 1:length(SNP_metaData_list)) {
+      if (i == 1){SNP_metaData <- SNP_metaData_list[[i]]} else {SNP_metaData <- rbind(SNP_metaData, SNP_metaData_list[[i]])}
+    }
+
+    chromatin_states_annotation_oi <- chromatin_states_annotation[seqnames(chromatin_states_annotation)%in% chrs,]
+
+    SNP_metaData_ChromStates_list <- parallel::mclapply(regions_list,
+                                                        DAPDNAm::chromatin_state_region_metaData_generation,
+                                                        chromatin_states_annotation = chromatin_states_annotation_oi,
+                                                        mc.cores=n_cores)
+
+    for (i in 1:length(SNP_metaData_ChromStates_list)) {
+      if (i == 1){SNP_metaData_ChromStates <- SNP_metaData_ChromStates_list[[i]]} else {SNP_metaData_ChromStates <- rbind(SNP_metaData_ChromStates, SNP_metaData_ChromStates_list[[i]])}
+    }
+
+    if (all(rownames(SNP_metaData_TE) == rownames(SNP_metaData))){SNP_metaData <- cbind(SNP_metaData, SNP_metaData_TE)} else {print("not all the transposable element rownames are the same!!!")}
+    if (all(rownames(SNP_metaData_ChromStates) == rownames(SNP_metaData))){SNP_metaData <- cbind(SNP_metaData, SNP_metaData_ChromStates)} else {print("not all the chromatin states rownames are the same!!!")}
+    # if (all(rownames(human_conserved_SNP_metaData) == rownames(SNP_metaData))){SNP_metaData <- cbind(SNP_metaData, human_conserved_SNP_metaData)} else {print("not all the human conserved regions rownames are the same!!!")}
+
+    # Identify columns with the name "region"
+    region_cols <- which(names(SNP_metaData) == "region")
+
+    # Check if there are multiple "region" columns
+    while (length(region_cols) > 1) {
+      # Remove the second "region" column
+      SNP_metaData <- SNP_metaData[, -region_cols[2]]
+      # Identify columns with the name "region"
+      region_cols <- which(names(SNP_metaData) == "region")
+    }
+
+    SNP_metaData$chr_snp <- gsub("^([^_]*_[^_]*)_.*$", "\\1", SNP_metaData$region)
+    binding <- SNPs[SNPs$chr_snp %in% SNP_metaData$chr_snp,]
+    binding <- binding[order(binding$chr_snp),]
+    SNP_metaData <- SNP_metaData[order(SNP_metaData$chr_snp),]
+    if(!all(binding$chr_snp == SNP_metaData$chr_snp)){stop("fix yo shit")}
+    SNP_metaData<-cbind(SNP_metaData, binding[,c('chr','pos','ALT','REF')])
+
+    # rownames(SNP_metaData) <- SNP_metaData$region
+
+    all_results <- list()
+    # load("inprogress.RData")
+
+    library(VariantAnnotation)
+    library(parallel)
+    genome <- readDNAStringSet("/scratch/bmarine2/canFam4.fa", format = "fasta")
+
+    chrs <- unique(SNP_metaData$chr)
+    for (chr in chrs) {
+      print(chr)
+
+      chr_snps_oi <- unique(SNP_metaData$chr_snp[SNP_metaData$chr == chr])
+
+      # Parallel processing using mclapply
+      results <- mclapply(chr_snps_oi,
+                          function(chr_snp, SNP_metaData, chr, genome) {
+                            seq <- genome[[chr]]
+
+                            oi <- SNP_metaData[SNP_metaData$chr_snp==chr_snp,,drop=F]
+                            pos <- oi$pos
+                            pos1 <- oi$pos+1
+                            ref=oi$REF
+                            alt=oi$ALT
+
+                            posplus1REF <- as.character(subseq(seq, start = pos1, end = pos1))
+
+                            pos1 <- oi$pos-1
+                            posminus1REF <- as.character(subseq(seq, start = pos1, end = pos1))
+
+                            motif <- as.character(subseq(seq, start = pos-50, end = pos+50))
+                            motif_region <- paste0(chr, "_", pos-50, "_", pos+50)
+
+                            return_ <- unique(data.frame(chr_snp = chr_snp, REF = ref[1], ALT = alt[1],
+                                                         posplus1REF = posplus1REF[1],
+                                                         posminus1REF = posminus1REF[1],
+                                                         surr_motif = motif,
+                                                         stringsAsFactors = FALSE))
+
+                            return(return_)
+
+                          },  SNP_metaData, chr, genome,
+                          mc.cores = n_cores)
+
+      # Combine results into a single data frame
+      results <- do.call(rbind, results)
+
+      # Deduplicate rows based on `chr_snp`
+      results <- results[!duplicated(results$chr_snp), ]
+
+      # Append to the list of results
+      all_results[[chr]] <- results
+    }
+
+    if(length(chrs) == 1) {combined_results <- all_results[[1]]} else {
+      # Combine all results from all chromosomes
+      combined_results <- do.call(rbind, all_results)
+    }
+
+    # Remove duplicate rows if necessary
+    combined_results <- combined_results[!duplicated(combined_results$chr_snp), ]
+
+    # Merge combined results back into SNP_metaData
+    SNP_metaData <- merge(SNP_metaData, combined_results[,c("chr_snp", "posplus1REF", "posminus1REF", "surr_motif")], by = "chr_snp", all.x = TRUE)
+
+    CT_SNPs <- read.table("/scratch/bmarine2/CpG_gain_aware_fasta-250103/edited_snps.txt", header = F)
+
+    SNP_metaData$CpG_disrupting <- "No"
+    SNP_metaData$CpG_disrupting[SNP_metaData$chr_region %in% CT_SNPs$V1] <- "Yes"
+
+    SNP_metaData$C_to_T <- "No"
+    SNP_metaData$C_to_T[(SNP_metaData$REF == "C" & SNP_metaData$ALT == "T" | SNP_metaData$REF == "G" & SNP_metaData$ALT == "A")
+    ] <- "Yes"
+
+    write.table(x = SNP_metaData, file = paste0("SNP_metaData/chr",chr,"_SNP_batch",j,".tsv"))
+  }
+}
+
+
+
